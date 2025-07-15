7701973f6ebea5a424167dfe8a791d8216b1a394
Abhinav-2101 <abhinavkumarv2101@gmail.com>
2025-07-15 08:26:24 +0530

    Program2

---
+++ Program2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main{
+  public static void main(String args[])
+  {
+    Scanner s1=new Scanner(System.in);
+    System.out.print("Enter the number & name");
+    int s=s1.nextInt();
+    String s2=s1.nextLine();
+    System.out.print("number-"+ s);
+    System.out.print(" name-"+ s2);
+      }
+}
