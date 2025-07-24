82b9fb0b64aaf8b18b0e70eed44830dd66dfdfd2
Abhinav <83628416+abhinavuser@users.noreply.github.com>
2025-07-24 09:41:48 +0530

    Create Program6.java

---
+++ Program6.java
@@ -0,0 +1,9 @@
+import java.util.*t; 
+public class Main(
+for(intj=(i*);/<num;)=]+)( booll] = false;
+public static vold maln(String args[) ( Scanner sc = new Scanner(System.in); System.out.println('Enter a number); int num = sc.nextint(;
+`boolean[ bool = new boolean|[num)]: for (int i= 0; i< bool.length; itt).( bool[i true
+System.out printn("List of prime numbers upto given number are: ");
+for (int \= 2;1< bool.length; i++) ( if(bool[i)==true) (
+for (int i =2; l< Math.sqrt(num);i1++) if(bool[] == true) ( L DL
+System.out.printin@ 0
