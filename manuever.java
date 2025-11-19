bde636f7516703d6e0307d82f2fd765dbc4a0bde
Abhinav <83628416+abhinavuser@users.noreply.github.com>
2025-11-19 20:29:51 +0530

    Create manuever.java

---
+++ manuever.java
@@ -0,0 +1,13 @@
+public class EthCode {
+
+    static int numberOfPaths(int m, int n) {
+        if (m == 1 || n == 1)
+            return 1;
+
+        return numberOfPaths(m - 1, n) + numberOfPaths(m, n - 1);
+    }
+
+    public static void main(String args[]) {
+        System.out.println(numberOfPaths(5, 2));  // Output: 6
+    }
+}
